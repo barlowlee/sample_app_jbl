6e6f7e757079388fdad946c7e24ee68c7ed1fb33
Jonathan Lee <jlee@ctgla.org>
2011-07-31 10:54:09 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleAppJbl::Application.config.secret_token = '6eac9814ab2c66b64408f39aab81e299d288a3ad6e71e87b8a174157d7b18f0a6da5cef7689fdb41838dd8aff9f174ff9d643708a9d41f884fc2ecac26c8f01d'
