e80f9405beb7d9b7413dc6f41a6fd9e61c4f35e3
Jonathan Lee <jlee@ctgla.org>
2011-08-17 22:21:12 -0700

    Half way through lesson 4

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,9 +3,14 @@ module ApplicationHelper
   def title
     base_title =  "Ruby on Rails Tutorial Sample App"
     if @title.nil?
-      base_title
+      base_title   # Ruby functions return the last value
     else
-      "#{base_title} | #{@title}"
+      "#{base_title} | #{@title}"  # this is called "string interpolation -
+                                   #  putting  string variable in the middle of a string.
+                                   #  interpolating a nil variable produces the empty string, not nil
     end
   end
+  # A helper creates methods that can be called by views through embedded Ruby
+  #    in this case 'title' will be called by 'application.html.erb' which is the
+  #    main layout file for this site.
 end
